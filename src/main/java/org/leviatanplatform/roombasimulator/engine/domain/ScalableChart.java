5b15fdabc13d5b4ad4e996b92a90fedbad633ce8
CiudadAlex <ciudad.alex@gmail.com>
2026-05-15 18:38:08 +0200

    RoombaSimulator

--- src/main/java/org/leviatanplatform/roombasimulator/engine/domain/ScalableChart.java
+++ src/main/java/org/leviatanplatform/roombasimulator/engine/domain/ScalableChart.java
@@ -2,14 +2,32 @@ package org.leviatanplatform.roombasimulator.engine.domain;
 
 public class ScalableChart {
 
-    private Cell[][] chart;
+    private final int rows;
+    private final int columns;
+    private final Cell[][] chart;
+    private Position position;
+
+    public ScalableChart() {
+        this(10, 10);
+    }
 
     public ScalableChart(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
         this.chart = new Cell[rows][columns];
+        this.position = new Position(rows/2, columns/2);
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < columns; c++) {
-                this.chart[r][c] = new Cell();
+        fillNullCells();
+    }
+
+    private void fillNullCells() {
+
+        for (int r = 0; r < this.rows; r++) {
+            for (int c = 0; c < this.columns; c++) {
+
+                if (this.chart[r][c] == null) {
+                    this.chart[r][c] = new Cell();
+                }
             }
         }
     }
