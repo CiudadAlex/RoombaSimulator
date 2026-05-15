6ef1ac58c30960c82f07e5f96837e71f877be141
CiudadAlex <ciudad.alex@gmail.com>
2026-05-15 19:01:39 +0200

    RoombaSimulator

--- src/main/java/org/leviatanplatform/roombasimulator/engine/domain/Cell.java
+++ src/main/java/org/leviatanplatform/roombasimulator/engine/domain/Cell.java
@@ -4,5 +4,29 @@ public class Cell {
 
     private boolean stepped;
     private boolean actuated;
+    private boolean wall;
 
+    public boolean isStepped() {
+        return stepped;
+    }
+
+    public void setStepped(boolean stepped) {
+        this.stepped = stepped;
+    }
+
+    public boolean isActuated() {
+        return actuated;
+    }
+
+    public void setActuated(boolean actuated) {
+        this.actuated = actuated;
+    }
+
+    public boolean isWall() {
+        return wall;
+    }
+
+    public void setWall(boolean wall) {
+        this.wall = wall;
+    }
 }
--- src/main/java/org/leviatanplatform/roombasimulator/engine/domain/ScalableChart.java
+++ src/main/java/org/leviatanplatform/roombasimulator/engine/domain/ScalableChart.java
@@ -32,5 +32,49 @@ public class ScalableChart {
         }
     }
 
-    // FIXME finish
+    private void copyCells(Cell[][] externalChart, int initRow, int endRowExcluded, int initColumn, int endColumnExcluded, int deltaRow, int deltaColumn) {
+
+        for (int r = initRow; r < endRowExcluded; r++) {
+            for (int c = initColumn; c < endColumnExcluded; c++) {
+                this.chart[r+deltaRow][c+deltaColumn] = externalChart[r][c];
+            }
+        }
+    }
+
+    private void setPosition(int row, int column) {
+        this.position = new Position(row, column);
+    }
+
+    public ScalableChart scaleUp(int rowsToAdd) {
+
+        ScalableChart newScalableChart = new ScalableChart(rows + rowsToAdd, columns);
+        newScalableChart.copyCells(this.chart, 0, rows, 0, columns, rowsToAdd, 0);
+        newScalableChart.setPosition(this.position.getRow() + rowsToAdd, this.position.getColumn());
+        return newScalableChart;
+    }
+
+    public ScalableChart scaleDown(int rowsToAdd) {
+
+        ScalableChart newScalableChart = new ScalableChart(rows + rowsToAdd, columns);
+        newScalableChart.copyCells(this.chart, 0, rows, 0, columns, 0, 0);
+        newScalableChart.setPosition(this.position.getRow(), this.position.getColumn());
+        return newScalableChart;
+    }
+
+    public ScalableChart scaleLeft(int columnsToAdd) {
+
+        ScalableChart newScalableChart = new ScalableChart(rows, columns + columnsToAdd);
+        newScalableChart.copyCells(this.chart, 0, rows, 0, columns, 0, columnsToAdd);
+        newScalableChart.setPosition(this.position.getRow(), this.position.getColumn() + columnsToAdd);
+        return newScalableChart;
+    }
+
+    public ScalableChart scaleRight(int columnsToAdd) {
+
+        ScalableChart newScalableChart = new ScalableChart(rows, columns + columnsToAdd);
+        newScalableChart.copyCells(this.chart, 0, rows, 0, columns, 0, 0);
+        newScalableChart.setPosition(this.position.getRow(), this.position.getColumn());
+        return newScalableChart;
+    }
+
 }
