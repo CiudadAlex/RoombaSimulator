39f2da04c51e0faeb9c8ab0929fc84560723b4d4
CiudadAlex <ciudad.alex@gmail.com>
2026-05-18 00:01:06 +0200

    RoombaSimulator

--- src/main/java/org/leviatanplatform/roombasimulator/exampleenvironments/RectangleEnvironment.java
+++ src/main/java/org/leviatanplatform/roombasimulator/exampleenvironments/RectangleEnvironment.java
@@ -4,43 +4,41 @@ import org.leviatanplatform.roombasimulator.engine.domain.Environment;
 import org.leviatanplatform.roombasimulator.engine.domain.Movement;
 import org.leviatanplatform.roombasimulator.engine.domain.MovementResult;
 import org.leviatanplatform.roombasimulator.engine.domain.Position;
+import org.leviatanplatform.roombasimulator.exampleenvironments.utils.HorizontalRange;
+import org.leviatanplatform.roombasimulator.exampleenvironments.utils.MovementStatus;
+import org.leviatanplatform.roombasimulator.exampleenvironments.utils.VerticalRange;
 
 public class RectangleEnvironment implements Environment {
 
     private Position position = new Position(0, 0);
 
-    private final int halfRows;
-    private final int halfColumns;
+    private final VerticalRange verticalRange;
+    private final HorizontalRange horizontalRange;
 
     public RectangleEnvironment(int rows, int columns) {
-        halfRows = rows / 2;
-        halfColumns = columns / 2;
+        int halfRows = rows / 2;
+        int halfColumns = columns / 2;
+
+        verticalRange = new VerticalRange(halfRows, -halfRows, -halfColumns, halfColumns);
+        horizontalRange = new HorizontalRange(halfRows, -halfRows, -halfColumns, halfColumns);
     }
 
     @Override
     public MovementResult tryToMove(Movement movement) {
 
-        Position positionMoved = this.position.clonePosition();
-        positionMoved.move(movement);
-
-        int row = positionMoved.getRow();
-        int column = positionMoved.getColumn();
-
         switch (movement) {
             case UP, DOWN -> {
-                if (row < -halfRows || row > halfRows) {
-                    return MovementResult.WALL;
-                } else {
-                    this.position = positionMoved;
-                    return MovementResult.SUCCESS;
+                if (verticalRange.applies(position)) {
+                    MovementStatus movementStatus = verticalRange.evaluateMovement(position, movement);
+                    this.position = movementStatus.getNewPosition();
+                    return movementStatus.getMovementResult();
                 }
             }
             case LEFT, RIGHT -> {
-                if (column < -halfColumns || column > halfColumns) {
-                    return MovementResult.WALL;
-                } else {
-                    this.position = positionMoved;
-                    return MovementResult.SUCCESS;
+                if (horizontalRange.applies(position)) {
+                    MovementStatus movementStatus = horizontalRange.evaluateMovement(position, movement);
+                    this.position = movementStatus.getNewPosition();
+                    return movementStatus.getMovementResult();
                 }
             }
         }
