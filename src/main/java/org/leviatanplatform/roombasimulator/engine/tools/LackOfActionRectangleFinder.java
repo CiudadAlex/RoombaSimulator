d9cb9c335af43ebc6e3899ab6cb555815c3aeb74
CiudadAlex <ciudad.alex@gmail.com>
2026-05-21 01:00:38 +0200

    RoombaSimulator

--- src/main/java/org/leviatanplatform/roombasimulator/engine/tools/LackOfActionRectangleFinder.java
+++ src/main/java/org/leviatanplatform/roombasimulator/engine/tools/LackOfActionRectangleFinder.java
@@ -4,6 +4,8 @@ import org.leviatanplatform.roombasimulator.engine.domain.Position;
 import org.leviatanplatform.roombasimulator.engine.domain.Rectangle;
 import org.leviatanplatform.roombasimulator.engine.domain.ScalableChart;
 import org.leviatanplatform.roombasimulator.engine.utils.ChartUtils;
+
+import java.util.List;
 import java.util.Set;
 
 public class LackOfActionRectangleFinder {
@@ -11,7 +13,7 @@ public class LackOfActionRectangleFinder {
     public static Rectangle findBiggestRectangle(Position position, ScalableChart chart) {
 
         Set<Position> setNotWallPositions = ChartUtils. getAllPositionsInsideWallFromGivenPosition(position, chart);
-
+        List<Position> listNotActuatedPositions = setNotWallPositions.stream().filter(p -> !chart.getCellInfo(p).isActuated()).toList();
 
         // FIXME finish
         return null;
